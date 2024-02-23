e5b414b3e689543aaaad1489dfbe19c7f35d325f
Addison Howe (from Quest) <addi.howe@gmail.com>
2024-02-23 17:10:05 -0600

    added comments to losses

--- plnn/loss_functions.py
+++ plnn/loss_functions.py
@@ -9,6 +9,7 @@ Provides functions:
 
 import jax
 import jax.numpy as jnp
+from jaxtyping import Array, Float
 
 def select_loss_function(key)->callable:
     """Loss function selector method.
@@ -34,8 +35,14 @@ def select_loss_function(key)->callable:
 ##  Loss Functions  ##
 ######################
     
-def kl_divergence_loss(q_samps, p_samps) -> float:
+def kl_divergence_loss(
+        q_samps: Float[Array, "b m d"], 
+        p_samps: Float[Array, "b n d"],
+) -> Float:
     """Estimate the KL divergence. Returns the average over all batches.
+
+    We make the first argument q, rather than p, since we want our loss 
+    functions to take as first argument the approximation distribution.
 
     Adapted from:
       https://gist.github.com/atabakd/ed0f7581f8510c8587bc2f41a094b518
@@ -48,7 +55,7 @@ def kl_divergence_loss(q_samps, p_samps) -> float:
     Returns:
         (float) KL estimate of D(P||Q), averaged across batches.
     """
-    return jnp.mean(jax.vmap(kl_divergence_est)(q_samps, p_samps))
+    return jnp.mean(jax.vmap(kl_divergence_est)(p_samps, q_samps))
 
 def mean_cov_loss(y_sim, y_obs) -> float:
     """Loss function based on the difference of first and second moments.
@@ -98,21 +105,37 @@ def batch_cov(batch_points):
     """
     return jax.vmap(jnp.cov, 0)(batch_points.transpose((0, 2, 1)))
 
-def cdist(x, y):
+def cdist(
+        x: Float[Array, "n d"], 
+        y: Float[Array, "m d"], 
+) -> Float[Array, "n m"]:
+    """Return pairwise distances between n and m d-dimensional vectors.
+
+    Args:
+        x (Array) : Shape (n,d)
+        y (Array) : Shape (m,d)
+
+    Returns:
+        (Array) : Shape (n,m). Matrix of pairwise distances with each row 
+            corresponding to an element of x and each column to an element of y.
+    """
     return jax.vmap(lambda x1: jax.vmap(
         lambda y1: euclidean_distance(x1, y1))(y))(x)
 
-def kl_divergence_est(q_samp, p_samp):
+def kl_divergence_est(
+        p_samp: Float[Array, "n d"],
+        q_samp: Float[Array, "m d"], 
+) -> Float:
     """Estimate the KL divergence.
     
     Adapted from:
       https://gist.github.com/atabakd/ed0f7581f8510c8587bc2f41a094b518
 
     Args:
-        q_samp (array) : Samples from estimated (i.e. approximate) distribution.
-            Shape (m,d).
-        p_samp (array) : Samples from target (i.e. true) distribution. 
-            Shape (n,d).
+        p_samp (array) : Samples from target (i.e. true) distribution P, and 
+            denoted by X. Shape (n,d).
+        q_samp (array) : Samples from estimated (i.e. approximate) 
+            distribution Q, and denoted by Y. Shape (m,d).
     Returns:
         (float) KL estimate of D(P||Q).
     """
@@ -120,10 +143,19 @@ def kl_divergence_est(q_samp, p_samp):
     n, d = p_samp.shape
     m, _ = q_samp.shape
     
-    diffs_xx = cdist(p_samp, p_samp)
-    diffs_xy = cdist(p_samp, q_samp)
-    
+    # Compute r(xi) and s(xi) for each of the n points xi in X ~ p(x).
+    # r(xi) is the distance of xi to its nearest neighbor in the set X\{xi}.
+    # s(xi) is the distance of xi to its nearest neighbor in the set Y.
+
+    # Start by computing pairwise distances d(X,X) and d(X,Y)
+    diffs_xx = cdist(p_samp, p_samp)  # diffs_xx[i,j] = d(xi, xj)
+    diffs_xy = cdist(p_samp, q_samp)  # diffs_xy[i,j] = d(xi, yj)
+
+    # top_k(D, k)[0] returns an array of shape (D.shape[0], k). Each row 
+    # corresponds to a row of D, with top k elements in decreasing order.
+    # The double negatives thus smallest k values, in increasing order.
+    # For dist(X,X), the smallest will be 0, and we want the second smallest.
     r = -jax.lax.top_k(-diffs_xx, 2)[0][:,1]
     s = -jax.lax.top_k(-diffs_xy, 1)[0][:,0]
-    lossval = -jnp.log(r/s).sum() * d/n + jnp.log(m / (n - 1.))
+    lossval = jnp.log(m/(n-1.)) - jnp.log(r/s).sum() * d/n
     return lossval
