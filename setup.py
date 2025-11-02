1a6ebc2806744d4979021a7d41516ea128afdec5
Addison Howe <addi.howe@gmail.com>
2025-11-02 11:46:41 -0600

    refactor into src directory

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="plnn",
-    version="0.1.0",
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "plnn = plnn.__main__:main",
-            "model_eval = plnn.eval.__main__:main",
-            "generate_data = plnn.data_generation.__main__:main",
-        ]
-    },
+    packages=find_packages("src"),
+    package_dir={"": "src"},
 )
